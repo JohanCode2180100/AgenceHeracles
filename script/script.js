b75a2e4d5b999f3e8a4baa560aa6b8d9c80f417c
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-01-18 11:18:53 +0100

    add page 'informations' and add a media queries

--- script/script.js
+++ script/script.js
@@ -1,14 +1,21 @@
+// recupere tout les elements du DOM
+
 const itemsSlide = document.querySelectorAll(".container-slides img");
 const nbSlides = itemsSlide.length;
-const afterSlide = document.querySelector("right");
-const beforeSlide = document.querySelector("left");
+const afterSlide = document.querySelector(".right");
+const beforeSlide = document.querySelector(".left");
+
+//init le compteur de defilement a 0
 let count = 0;
 
+//ecoute du clic sur afterSlide
 afterSlide.addEventListener("click", after_Slide);
 
+//execute la function after_slide
 function after_Slide() {
   itemsSlide[count].classList.remove("active");
 
+  //count inferieur a la taille du tableau 3 - 1
   if (count < nbSlides - 1) {
     count++;
   } else {
@@ -17,6 +24,7 @@ function after_Slide() {
   itemsSlide[count].classList.add("active");
 }
 
+//ecoute du clic sur beforeSlide
 beforeSlide.addEventListener("click", before_Slide);
 
 function before_Slide() {
@@ -29,3 +37,4 @@ function before_Slide() {
   }
   itemsSlide[count].classList.add("active");
 }
+console.log(nbSlides);
